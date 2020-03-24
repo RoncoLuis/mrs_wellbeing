6c0ad135b213b7a34d325795c06ad8176fe03267
rasp_tesla <w580ix@gmail.com>
2020-03-23 21:43:07 -0600

    convierte la voz a texto. Hace el append al archivo sin modificar lo escrito

---
+++ README.md
@@ -0,0 +1,3 @@
+# Módulo tjbot
+Módulo de procesamiento de voz a texto usando API´s de IBM Watson
+para el proyecto de investigación MCC.
--- app.js
+++ app.js
@@ -3,38 +3,31 @@ const tjfunctions = require('./tjbot-config/tjFunctions');
 const TJBot = require('tjbot');
 const colors = require('colors');
 
-var tj = new TJBot(tjconfig.hardware, tjconfig.tjbot_config, tjconfig.ibm_credentials); //instancia TJBot
+var tj = new TJBot(tjconfig.hardware, tjconfig.tjbot_config, tjconfig.ibm_credentials);
 var tj_name = tjconfig.tjbot_config.robot.name;
-console.log(colors.yellow(`Hola. Mi nombre es ${tj_name}. Estoy listo para comenzar, para detenerme solo di mi nombre`));
+
+var filePath = tjfunctions.create_file("luiseduardo");
+console.log("TJBot ahora esta escuchando...");
 
 tj.listen((msg) => {
-    if (msg.toLowerCase().startsWith(tj_name.toLowerCase())) {
-        console.log('perfecto dijiste mi nombre correctamente. Esta fue la conversacion:');
-        data = tjfunctions.getListado();
-        console.log(data);
-        tj.translate(JSON.stringify(data), "es", "en").then((response) => {
-            console.log(response.translations[0].translation);
-            var newData = response.translations[0].translation;
-            var translate = tj.analyzeTone(newData).then(resp => {
-                console.log(resp);
-            });
-        });
+    let voiceToTxt = msg.toLowerCase(); //todo el texto se convierte a minusculas
+    if (voiceToTxt.startsWith(tj_name.toLowerCase())) {
+        console.log('Perfecto se reconocion la palabra', tj_name);
+        // data = tjfunctions.getListado();
+        // console.log(data);
+        // tj.translate(JSON.stringify(data), "es", "en").then((response) => {
+        //     console.log(response.translations[0].translation);
+        //     var newData = response.translations[0].translation;
+        //     var translate = tj.analyzeTone(newData).then(resp => {
+        //         console.log(resp);
+        //     });
+        // });
     }
-    try {
-        var txt = msg.toLowerCase();
-        if (txt.length > 5) {
-            tjfunctions.crear(msg.toLowerCase());
-            console.log(txt);
-        } else {
-            console.log(msg.toLowerCase(), ' ==== No analizado');
-        }
-    } catch (error) {
-        console.log("mi error", error);
+
+    if (voiceToTxt.length > 10) {
+        tjfunctions.fill_conversation(voiceToTxt, filePath);
+        console.log(voiceToTxt);
+    } else {
+        console.log(voiceToTxt, ' ==== No analizado..');
     }
 });
-
-// perform sentiment analysis every N seconds
-// setInterval(function() {
-//     console.log("Performing sentiment analysis of the tweets");
-//     shineFromTweetSentiment();
-// }, SENTIMENT_ANALYSIS_FREQUENCY_MSEC);
